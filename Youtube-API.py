b81cbb15be865d333cf231bb62684f3dc2113471
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-11-18 04:31:02 -0500

    Update Youtube-API.py

--- Youtube-API.py
+++ Youtube-API.py
@@ -1,9 +1,9 @@
 from googleapiclient.discovery import build
 
-api_key = 'AIzaSyA-fDSFGhySHwNcQMixvge_BSuI8xC-gsY'
+api_key = ''
 
 youtube = build('youtube', 'v3', developerKey=api_key)
-channel_id = 'UC8Wx__5E_zUR8jQ4rkHMw-w'
+channel_id = ''
 
 response = youtube.channels().list(
     part='statistics',
@@ -21,27 +21,6 @@ view_count = stats.get('viewCount', 'Unknown')
 print("Subscribers:",int(subscriber_count))
 print("Videos on channel:",int(video_count))
 print("Total Views:",int(view_count))
-
-# from googleapiclient.discovery import build
-#
-# api_key = 'AIzaSyA-fDSFGhySHwNcQMixvge_BSuI8xC-gsY'
-#
-# youtube = build('youtube', 'v3', developerKey=api_key)
-# channel_id = 'UC8Wx__5E_zUR8jQ4rkHMw-w'
-#
-# # Retrieve the user IDs of subscribers
-# response = youtube.subscriptions().list(
-#     part='subscriberSnippet',
-#     mySubscribers=True,
-#     maxResults=50  # Adjust as needed
-# ).execute()
-#
-# # Extract the user IDs from the response
-# subscriber_ids = [item['subscriberSnippet']['resourceId']['channelId'] for item in response.get('items', [])]
-#
-# # Print the subscriber IDs
-# for idx, subscriber_id in enumerate(subscriber_ids, start=1):
-#     print(f"Subscriber {idx}: {subscriber_id}")
 
 
 
